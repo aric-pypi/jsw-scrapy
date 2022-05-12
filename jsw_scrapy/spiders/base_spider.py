4cda0490decd4b46fa3e15f6f4dd3b81fef22e99
feizheng <1290657123@qq.com>
2022-05-13 06:32:05 +0800

    feat: add support_crawling

--- jsw_scrapy/spiders/base_spider.py
+++ jsw_scrapy/spiders/base_spider.py
@@ -10,25 +10,44 @@ class BaseSpider(scrapy.Spider):
     ua_pc = 'Mozilla/5.0 zgrab/0.x'
     ua_mobile = 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_1 like Mac OS X) By aric.zheng/0.x'
     ua_random = ua.chrome
+    support_crawling = False
 
-    def get_un_crawled(self, **kwargs):
+    @classmethod
+    def is_crawled_complete(cls, **kwargs):
+        entity_class = kwargs.get('entity_class')
+        crawled = kwargs.get('crawled', {'is_crawled': False})
+        return entity_class.where(crawled).count() == 0
+
+    @classmethod
+    def get_un_crawled(cls, **kwargs):
         # required:
         entity_class = kwargs.get('entity_class')
-        # optional:
+        # crawl + optional:
         crawled = kwargs.get('crawled', {'is_crawled': False})
-        options = crawled.merge(kwargs.get('options', {}))
+        crawling_opts = {'is_crawling': False} if cls.support_crawling else {}
+        options = crawled.merge(kwargs.get('options', crawling_opts))
         limit = kwargs.get('limit', None)
 
         if limit:
-            return entity_class.where(options).take(limit).get()
+            records = entity_class.where(options).take(limit).get()
         else:
-            return entity_class.where(options).get()
+            records = entity_class.where(options).get()
 
-    def update_crawled(self, **kwargs):
+        # start crawling, update is_crawling
+        if cls.support_crawling:
+            for record in records:
+                record.is_crawling = True
+                record.save()
+
+        return records
+
+    @classmethod
+    def update_crawled(cls, **kwargs):
         record = kwargs.get('record')
         crawled = kwargs.get('crawled', 'is_crawled')
         setattr(record, crawled, True)
         record.save()
 
-    def noop_request(self):
+    @classmethod
+    def noop_request(cls):
         yield scrapy.Request(url="https://www.baidu.com", callback=nx.noop_scrapy_parse)
