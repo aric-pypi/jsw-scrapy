f52255e1c3161e9b94cbc1849adc531cc4012bc3
feizheng <1290657123@qq.com>
2022-05-27 16:49:22 +0800

    feat: upt jsw-scrap models

--- jsw_scrapy/__init__.py
+++ jsw_scrapy/__init__.py
@@ -3,6 +3,8 @@ import pkg_resources
 version = pkg_resources.get_distribution('jsw-scrapy').version
 __version__ = version
 
-# next pipelines/spiders
+# next models/pipelines/spiders
+from jsw_scrapy.models import BaseModel
 from jsw_scrapy.pipelines.base_pipeline import BasePipeline
 from jsw_scrapy.spiders.base_spider import BaseSpider
+
