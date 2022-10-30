f376d718c2ca850da55d6c13c85f60801ecb7276
aric <1290657123@qq.com>
2022-10-30 16:54:44 +0800

    wip

--- jsw_scrapy/models/base_peewee_model.py
+++ jsw_scrapy/models/base_peewee_model.py
@@ -3,6 +3,5 @@ from peewee import *
 
 
 class BasePeeweeModel(Model):
-    is_crawled = BooleanField()
     created_at = DateTimeField(default=datetime.datetime.now())
     updated_at = DateTimeField(default=datetime.datetime.now())
