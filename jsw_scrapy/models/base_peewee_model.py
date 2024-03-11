cd9560b51257a41bf5164cd88e9ebaad55ec070f
aric <1290657123@qq.com>
2024-03-11 21:16:19 +0800

    feat: rm unused pkg

--- jsw_scrapy/models/base_peewee_model.py
+++
@@ -1,7 +0,0 @@
-import datetime
-from peewee import *
-
-
-class BasePeeweeModel(Model):
-    created_at = DateTimeField(default=datetime.datetime.now())
-    updated_at = DateTimeField(default=datetime.datetime.now())
